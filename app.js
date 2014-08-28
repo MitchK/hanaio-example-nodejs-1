9f68acaf54541b2f74379d74e443dddfcb8b14fc
Michael Kunzmann <michael.kunzmann@sap.com>
2014-08-27 21:32:46 -0700

    app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+// modules
+var http = require('http')
+  , hdb = require('hdb');
+
+// client
+var client = hdb.createClient({
+  host: process.env.HANA_HOST, 
+  port: parseInt(process.env.HANA_PORT), 
+  user: process.env.HANA_USER, 
+  password: process.env.HANA_PASS
+});
+
+// server
+http.createServer(function (req, res) {
+    client.connect(function (err) {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Connection failed! ' + err);
+            return;
+        } 
+        client.exec('SELECT CURRENT_TIMESTAMP "time" FROM DUMMY', function (err, rows) {
+            client.end();
+            if (err) {
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.end('Execution failed! ' + err);
+                return;
+            }
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(rows));
+        });
+    });
+}).listen(8080);
+
+console.log("example-nodejs-1 is listening. ".bold + "Open the endpoint from the HANA.IO dashboard.");
